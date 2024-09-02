a5712a5524db0911962f64cb41b7849153019ff5
Legokichi Duckscallion <legokichi@gmail.com>
2024-09-03 04:26:29 +0900

    wip

--- browser/src/components/map_component.rs
+++ browser/src/components/map_component.rs
@@ -36,43 +36,46 @@ pub fn map_component(Props { forcus, points }: &Props) -> Html {
             map.set_view(&LatLng::new(forcus.latitude, forcus.longitude), 11.0); // Fuji
             let opt = &leaflet::TileLayerOptions::new();
             opt.set_attribution("<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>".to_string());
-            let osm = TileLayer::new_options(
-                // "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
-                // "https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.png",
-                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            let gsi = TileLayer::new_options(
+                "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
                 opt,
             );
-            osm.add_to(&map);
-            
+            let opt = &leaflet::TileLayerOptions::new();
+            opt.set_attribution(r#"attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',"#.to_string());
+            let osm =
+                TileLayer::new_options("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", opt);
+            gsi.add_to(&map);
+
             let opt = js_sys::Object::new();
-            js_sys::Reflect::set(&opt, &JsValue::from("OpenStreetMap"), &JsValue::from(osm)).unwrap();
-            let control = leaflet::LayerControl::new(&opt);
+            js_sys::Reflect::set(&opt, &JsValue::from("OpenStreetMap"), &JsValue::from(osm))
+                .unwrap();
+            js_sys::Reflect::set(&opt, &JsValue::from("地理院タイル"), &JsValue::from(gsi))
+                .unwrap();
+
+            let control = leaflet::LayersControl::new(&opt);
             control.add_to(&map);
 
             map_state.set(Some(map));
-            console::log!(format!("{:?}", map_state));
         }
     });
     // pointsが変化したら再描画
     use_effect_with(points.clone(), {
         let map_state = map_state.clone();
         move |points| {
-            console::log!("sapodposakd");
-            console::log!(format!("{:?}", map_state));
             if let Some(map) = map_state.as_ref() {
                 for point in points {
-                    let opt = leaflet::IconOptions::new();
-                    opt.set_icon_url("marker-red.png".to_string());
-                    opt.set_icon_size(leaflet::Point::new(25.0, 41.0));
-                    opt.set_icon_anchor(leaflet::Point::new(12.0, 40.0));
-                    opt.set_popup_anchor(leaflet::Point::new(0.0, -40.0));
-                    let my_icon = leaflet::Icon::new(&opt);
-                    let opt = leaflet::MarkerOptions::new();
-                    opt.set_icon(my_icon);
-                    leaflet::Marker::new_with_options(
-                        // leaflet::Marker::new(
+                    // let opt = leaflet::IconOptions::new();
+                    // opt.set_icon_url("marker-red.png".to_string());
+                    // opt.set_icon_size(leaflet::Point::new(25.0, 41.0));
+                    // opt.set_icon_anchor(leaflet::Point::new(12.0, 40.0));
+                    // opt.set_popup_anchor(leaflet::Point::new(0.0, -40.0));
+                    // let my_icon = leaflet::Icon::new(&opt);
+                    // let opt = leaflet::MarkerOptions::new();
+                    // opt.set_icon(my_icon);
+                    // leaflet::Marker::new_with_options(
+                    leaflet::Marker::new(
                         &LatLng::new(point.latitude, point.longitude),
-                        &opt,
+                        // &opt,
                     )
                     .add_to(map);
                 }
@@ -83,16 +86,12 @@ pub fn map_component(Props { forcus, points }: &Props) -> Html {
     use_effect_with(forcus.clone(), {
         let map_state = map_state.clone();
         move |forcus| {
-            console::log!("spoakdak");
-            console::log!(format!("{:?}", map_state));
             if let Some(map) = map_state.as_ref() {
-                console::log!("spoakdak2");
                 map.set_view(&LatLng::new(forcus.latitude, forcus.longitude), 11.0);
             }
         }
     });
-    // ここに変化なければ再描画されない
-    console::log!("aasdfada");
+    // この VDOM に変化なければ再描画されない
     html! {
         <div id="map" ref={node_ref}>
         </div>
