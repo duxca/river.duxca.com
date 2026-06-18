af319e96fe706c587ce6843477e4194c93eae6a3
Legokichi Duckscallion <legokichi@gmail.com>
2026-06-18 13:15:34 +0900

    Provide server API context to app shell

--- server/src/web/app.rs
+++ server/src/web/app.rs
@@ -1,12 +1,26 @@
-#[tracing::instrument(level = "trace", skip(st, req))]
+#[tracing::instrument(level = "trace", skip(auth_session, st, req))]
 pub async fn app_shell(
+    auth_session: axum_login::AuthSession<crate::web::login::Backend>,
     axum::extract::State(ref st): axum::extract::State<crate::web::State>,
     req: axum::http::Request<axum::body::Body>,
 ) -> Result<impl axum::response::IntoResponse, crate::web::Ise> {
     use axum::response::IntoResponse;
+    use leptos::prelude::*;
 
     let options = st.leptos_options.clone();
-    let handler = leptos_axum::render_app_to_stream(move || app::shell(options.clone()));
+    let user = auth_session.user;
+    let db = st.db.clone();
+    let handler = leptos_axum::render_app_to_stream_with_context(
+        move || {
+            if let Some(user) = user.clone() {
+                provide_context(shared_api::ServerApiContext {
+                    db: db.clone(),
+                    user,
+                });
+            }
+        },
+        move || app::shell(options.clone()),
+    );
 
     Ok(handler(req).await.into_response())
 }
